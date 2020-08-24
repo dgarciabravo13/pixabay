11f1f9c1143ce158bf3e539d6a02d894b3658134
David Garcia <dgarciabravo13@gmail.com>
2020-08-24 16:29:26 +0200

    pager done

--- src/App.js
+++ src/App.js
@@ -1,41 +1,72 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
 import ListadoImagenes from "./components/ListadoImagenes";
 
 function App() {
-
   const [busqueda, guardarBusqueda] = useState("");
   const [imagenes, guardarImagenes] = useState([]);
-  const [paginaactual, guardarPaginaActual] = useState(1);
-  const [totalpaginas, guardarTotalPaginas] = useState(1);
+  const [paginaactual, guardarPaginaActual] = useState(6);
+  const [totalpaginas, guardarTotalPaginas] = useState(10);
 
-  useEffect(()=>{
-    if(busqueda === "") return;
-    const consultarApi = async () =>{
+  useEffect(() => {
+    if (busqueda === "") return;
+    const consultarApi = async () => {
       const imagenesPorPagina = 30;
-      const key= `${process.env.REACT_APP_KEY}`
-      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+      const key = `${process.env.REACT_APP_KEY}`;
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}&page=${paginaactual}`;
       const respuesta = await fetch(url);
       const resultado = await respuesta.json();
       guardarImagenes(resultado.hits);
       //guardar el total de paginas en el state guardarTotalPaginas
-      guardarTotalPaginas(Math.ceil(resultado.totalHits/imagenesPorPagina))
-    }
+      guardarTotalPaginas(Math.ceil(resultado.totalHits / imagenesPorPagina));
+
+      //Mover la pantalla hacia arriba
+
+      const jumbotron = document.querySelector(".jumbotron");
+      jumbotron.scrollIntoView({behavior:"smooth"});
+    };
     consultarApi();
+  }, [busqueda,paginaactual]);
 
-  },[busqueda])
+  //definir la pagina anterior
+  const paginaAnterior = () => {
+    const nuevaPaginaActual = paginaactual - 1;
+    if (nuevaPaginaActual === 0) return;
+    guardarPaginaActual(nuevaPaginaActual);
+  };
 
+  const paginaSiguiente = () => {
+    const nuevaPaginaActual = paginaactual + 1;
+    if (nuevaPaginaActual > totalpaginas) return;
+    guardarPaginaActual(nuevaPaginaActual);
+  };
 
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">Buscador de imágenes</p>
-        <Formulario
-          guardarBusqueda={guardarBusqueda}
-        />
+        <Formulario guardarBusqueda={guardarBusqueda} />
       </div>
       <div className="row justify-content-center">
-        <ListadoImagenes imagenes={imagenes}/>
+        <ListadoImagenes imagenes={imagenes} />
+        {paginaactual === 1 ? null : (
+          <button
+            type="button"
+            className="btn btn-info mr-1"
+            onClick={paginaAnterior}
+          >
+            &laquo; Anterior
+          </button>
+        )}
+        {paginaactual === totalpaginas ? null : (
+          <button
+            type="button"
+            className="btn btn-info mr-1"
+            onClick={paginaSiguiente}
+          >
+            Siguiente &raquo;
+          </button>
+        )}
       </div>
     </div>
   );
